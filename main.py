5a55912d4c15840c2fefda9a6874feae19ab98a5
Kurt Krafft <kkrafft6609@gmail.com>
2020-04-14 09:34:27 -0500

    student exercises challenge

--- main.py
+++ main.py
@@ -8,6 +8,14 @@ flexbox = Exercise('Flex-box Froggy', "CSS")
 itinerary = Exercise('Nashville Itinerary', "Javascript")
 nutshell = Exercise('First Nutshell', "Javascript")
 cars = Exercise('Cars', "Python")
+python_tutorial = Exercise('Python Tutorial', 'Python')
+react_project = Exercise('React Project', 'Javascript')
+c_sharp_gold = Exercise('C# Gold', 'C#')
+chicken_monkey = Exercise('Chicken_Monkey', 'Python')
+css_grid = Exercise('CSS Grid', 'CSS')
+UI_test = Exercise('UI_Test', 'CSS')
+word_doc = Exercise('Word Document', 'Microsoft Word')
+puzzles = Exercise('Puzzles', 'books')
 
 ##### Cohorts
 
@@ -17,10 +25,10 @@ cohort_39 = Cohort("Night Cohort 39", [], [] )
 
 ######Students
 
-jim = Student('Jim', 'Halpert', 'lil_jimmy', 38, "Python tutorial", "React project")
-dwight = Student('Dwight', "Shrute", 'beets_FTW', 37, 'C# gold', 'Chicken-Monkey')
-pam = Student('Pam', 'Beasley', 'white_shoes_4Eva', 39, 'Css-grid', 'UI test')
-creed = Student('Creed', "Bratton", 'www.creedthoughts.com/gov', 38, 'word-doc', 'puzzles')
+jim = Student('Jim', 'Halpert', 'lil_jimmy', 38, python_tutorial,react_project)
+dwight = Student('Dwight', "Shrute", 'beets_FTW', 37, c_sharp_gold, chicken_monkey)
+pam = Student('Pam', 'Beasley', 'white_shoes_4Eva', 39, css_grid, UI_test)
+creed = Student('Creed', "Bratton", 'www.creedthoughts.com/gov', 38, word_doc, puzzles)
 
 
 ###### Instructors
@@ -40,6 +48,24 @@ bryan.assign_exercise(itinerary, pam)
 
 michael.assign_exercise(cars, dwight)
 michael.assign_exercise(nutshell, dwight)
+
+
+#########Challenge
+students = [jim, dwight, pam, creed]
+
+for student in students:
+    exercises = student.exercises
+    last_one = exercises[-1]
+    exercises.pop(-1)
+    exercise_names = list()
+    for exercise in exercises:
+        exercise_names.append(exercise.name)
+
+    joint = ", ".join(exercise_names)
+    print(f"{student.first_name} is currently working on {joint} and {last_one.name} exercises")
+
+
+    
 
 
 
